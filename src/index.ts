0ac8c289bb1a5924439427c39b0090643f57760a
mythosmystery <axiysfire@gmail.com>
2021-11-03 01:20:04 -0500

    updated index file

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,6 @@ const main = async () => {
          }),
          cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-            sameSite: 'lax',
             httpOnly: true,
          },
          name: 'qid',
