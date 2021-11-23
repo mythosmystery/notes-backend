4f2beb429056802c837a530d03a7be015e62ec74
mythosmystery <axiysfire@gmail.com>
2021-11-23 01:28:55 -0600

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,12 @@ const main = async () => {
       introspection: true
    });
 
-   apolloServer.applyMiddleware({ app });
+   apolloServer.applyMiddleware({
+      app,
+      cors: {
+         origin: process.env.PROD_FRONTEND_URL
+      }
+   });
 
    app.listen(process.env.PORT || 3001, () => {
       console.log(`server started on http://localhost:3001/graphql`);
